74241e6ee81bab2577550843880ef670d26c9bc5
FRANCISCO <roshlandia@gmail.com>
2022-07-14 21:57:35 -0500

    arregloTell

--- src/componentes/Contacto.jsx
+++ src/componentes/Contacto.jsx
@@ -5,11 +5,11 @@ const Contacto = () => {
     <div>Contacto
      <div>
         <p>Telefono</p>
-        <img className='targetProyect' src="https://cdn-icons.flaticon.com/png/512/1207/premium/1207423.png?token=exp=1657769995~hmac=7fd0d07cd00d3ef2f0cacc2cc1084eb2" alt="Telefono" title='Telefono'/>
+        <img className='targetProyect' src="https://cdn-icons.flaticon.com/png/512/2511/premium/2511865.png?token=exp=1657854369~hmac=717d14ff7854df4cc70b9a8b4a28991f" alt="Telefono" title='Telefono'/>
         <p>+52-56-12-58-76-65</p>
      </div>
        <p>Correo</p>
-       <img className='targetProyect' src="https://cdn-icons.flaticon.com/png/512/2875/premium/2875394.png?token=exp=1657770878~hmac=20ceed5e75aa3f87b6ebf739414f1f89" alt="Correo" title='Correo'/>
+       <img className='targetProyect' src="https://cdn-icons.flaticon.com/png/512/3543/premium/3543419.png?token=exp=1657853647~hmac=795e3c4ff781f74e09b2c10db9ac3031" alt="Correo" title='Correo'/>
 
      <div>
 
